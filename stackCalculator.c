296020c20cba823b1dd5c53a8b28946aea95e6cd
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-31 16:47:23 -0400

    Add push function to push elements onto stack

--- stackCalculator.c
+++ stackCalculator.c
@@ -1,8 +1,28 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+//definition of linked list used to store stack
+struct node
+{
+    int data;
+    struct node *next;
+    int stackSize;
+};
+
+
+//function that pushes value onto top of stack by creating a new node
+//with the new value and this node points to the previous top node
+struct node *push(struct node *top, int data)
+{
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->next = top;
+    return newNode;
+}
+
 int main()
 {
+    struct node *top = NULL;
 
 
     return 0;
